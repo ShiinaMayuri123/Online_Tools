40e9ceb265ab5d81d7470b9e9f6f8e4de69411cb
ShiinaMayuri123 <xxp8888888@gmail.com>
2026-05-06 01:41:43 +0800

    配置自定义域名

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Online_Tools/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
